014a1d25e75c3004b0c0d70cb88f5ffd6b98ee00
Ryoneme2 <worknarair6@gmail.com>
2022-12-03 00:01:10 +0700

    remove unless method

--- src/main/java/com/example/demo/demo/Tictactoe/Controller.java
+++ src/main/java/com/example/demo/demo/Tictactoe/Controller.java
@@ -11,13 +11,6 @@ import com.example.demo.demo.Tictactoe.Board.Board;
 @RestController
 @RequestMapping(path = "api/v1/boards")
 public class Controller {
-  public <T> List<T> twoDArrayToList(T[][] twoDArray) {
-    List<T> list = new ArrayList<T>();
-    for (T[] array : twoDArray) {
-      list.addAll(Arrays.asList(array));
-    }
-    return list;
-  }
 
   @GetMapping
   public List<int[]> getBoardField() {
