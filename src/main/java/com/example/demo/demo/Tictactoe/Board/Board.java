f9e30293ee18eaf18dc95a387d12e5cf94dfb184
Ryoneme2 <worknarair6@gmail.com>
2022-12-02 23:58:45 +0700

    get board list

--- src/main/java/com/example/demo/demo/Tictactoe/Board/Board.java
+++ src/main/java/com/example/demo/demo/Tictactoe/Board/Board.java
@@ -4,6 +4,10 @@ public class Board {
 
   private int[][] field;
 
+  public Board() {
+    this.field = new int[3][3];
+  }
+
   public void setCoordinate(int x, int y) {
     field[x][y] = 1;
   }
--- src/main/java/com/example/demo/demo/Tictactoe/Controller.java
+++ src/main/java/com/example/demo/demo/Tictactoe/Controller.java
@@ -1,6 +1,6 @@
 package com.example.demo.demo.Tictactoe;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +11,18 @@ import com.example.demo.demo.Tictactoe.Board.Board;
 @RestController
 @RequestMapping(path = "api/v1/boards")
 public class Controller {
+  public <T> List<T> twoDArrayToList(T[][] twoDArray) {
+    List<T> list = new ArrayList<T>();
+    for (T[] array : twoDArray) {
+      list.addAll(Arrays.asList(array));
+    }
+    return list;
+  }
+
   @GetMapping
-  public List<int[][]> getBoardField() {
+  public List<int[]> getBoardField() {
     Board board = new Board();
     int[][] field = board.getField();
-    System.out.println(field);
-    return List.of();
+    return Arrays.stream(field).toList();
   }
 }
